d9b2ff61caea6d5702d3e89b74cfc0c709bebcbf
Francisco Gamino <franciscogamino@Franciscos-MacBook-Air.local>
2026-03-03 22:25:17 -0600

    Initial Spring Boot project with homepage

---
+++ src/main/java/com/francisco/ideallocationfinder/IdealLocationFinderApplication.java
@@ -0,0 +1,13 @@
+package com.francisco.ideallocationfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealLocationFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdealLocationFinderApplication.class, args);
+    }
+
+}
